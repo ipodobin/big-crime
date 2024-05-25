2eaf24e58cdeb1dfece16c504b122a2451540285
igor.podobinski <igor.podobinski@pl.markant.com>
2024-05-25 16:16:47 +0200

    added more s3 buckets

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -10,8 +10,8 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "big_crime_data_bucket" {
-  bucket = "big-crime-data"
+resource "aws_s3_bucket" "big_crime_landing_zone_bucket" {
+  bucket = "big-crime-landing-zone"
 
   tags = {
     Name        = "BigCrimeDataBucket"
@@ -19,17 +19,16 @@ resource "aws_s3_bucket" "big_crime_data_bucket" {
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "big_crime_public_access" {
-  bucket = aws_s3_bucket.big_crime_data_bucket.id
-
+resource "aws_s3_bucket_public_access_block" "big_crime_landing_zone_bucket_public_access" {
+  bucket = aws_s3_bucket.big_crime_landing_zone_bucket.id
   block_public_acls       = false
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_policy" "bucket_policy" {
-  bucket = aws_s3_bucket.big_crime_data_bucket.id
+resource "aws_s3_bucket_policy" "big_crime_landing_zone_bucket_bucket_policy" {
+  bucket = aws_s3_bucket.big_crime_landing_zone_bucket.id
 
   policy = <<EOF
   {
@@ -41,7 +40,45 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
         "Principal": "*",
         "Action": ["s3:GetObject","s3:PutObject"],
         "Resource": [
-          "arn:aws:s3:::big-crime-data/*"
+          "arn:aws:s3:::big-crime-landing-zone/*"
+        ]
+      }
+    ]
+  }
+EOF
+}
+
+resource "aws_s3_bucket" "big_crime_formatted_data_bucket" {
+  bucket = "big-crime-formatted-data"
+
+  tags = {
+    Name        = "BigCrimeDataBucket"
+    Environment = "dev"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "big_crime_formatted_data_bucket_public_access" {
+  bucket = aws_s3_bucket.big_crime_formatted_data_bucket.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "big_crime_formatted_data_bucket_bucket_policy" {
+  bucket = aws_s3_bucket.big_crime_formatted_data_bucket.id
+
+  policy = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "PublicReadWrite",
+        "Effect": "Allow",
+        "Principal": "*",
+        "Action": ["s3:GetObject","s3:PutObject"],
+        "Resource": [
+          "arn:aws:s3:::big-crime-formatted-data/*"
         ]
       }
     ]
