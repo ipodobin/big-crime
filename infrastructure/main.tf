ac9c18df31881255fcedf1d9f72858ad1d942fd9
igor.podobinski <igor.podobinski@pl.markant.com>
2024-05-25 09:53:10 +0200

    added tofu s3 policy creation

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -13,10 +13,38 @@ provider "aws" {
 resource "aws_s3_bucket" "big_crime_data_bucket" {
   bucket = "big-crime-data"
 
-  acl    = "private"
-
   tags = {
     Name        = "BigCrimeDataBucket"
     Environment = "dev"
   }
+}
+
+resource "aws_s3_bucket_public_access_block" "big_crime_public_access" {
+  bucket = aws_s3_bucket.big_crime_data_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.big_crime_data_bucket.id
+
+  policy = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "PublicReadWrite",
+        "Effect": "Allow",
+        "Principal": "*",
+        "Action": ["s3:GetObject","s3:PutObject"],
+        "Resource": [
+          "arn:aws:s3:::big-crime-data/*"
+        ]
+      }
+    ]
+  }
+EOF
 }
