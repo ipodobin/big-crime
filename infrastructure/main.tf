be5c4df0fb579f674933fc7bec5212b6bbdf4b0c
igor.podobinski <igor.podobinski@pl.markant.com>
2024-05-25 13:54:29 +0200

    added ec2 resource to plan

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -48,3 +48,27 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
   }
 EOF
 }
+
+resource "aws_vpc" "myvpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "myvpc"
+  }
+}
+
+resource "aws_subnet" "mysubnet" {
+  vpc_id     = aws_vpc.myvpc.id
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    Name = "mysubnet"
+  }
+}
+
+resource "aws_instance" "nifi" {
+  ami           = "ami-080e1f13689e07408"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.mysubnet.id
+  tags = {
+    Name = "nifi"
+  }
+}
