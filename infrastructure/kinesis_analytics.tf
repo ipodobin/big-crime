e25e528d75fc2dedbc8e7f8e6519c061e4c33256
igor.podobinski <igor.podobinski@pl.markant.com>
2024-06-04 13:15:10 +0200

    added glue catalog to studio notebook v3

--- infrastructure/kinesis_analytics.tf
+++ infrastructure/kinesis_analytics.tf
@@ -22,19 +22,20 @@ resource "aws_glue_catalog_database" "big_crime_catalog" {
   name = "big-crime-glue-db-for-stream"
 }
 
-resource "aws_kinesisanalyticsv2_application" "big_crime_flink" {
-  name                   = "big-crime-flink-application"
-#   runtime_environment    = "FLINK-1_18"
-  runtime_environment    = "ZEPPELIN-FLINK-3_0"
-  service_execution_role = "arn:aws:iam::780087431924:role/LabRole"
-  application_mode       = "INTERACTIVE"
-  application_configuration {
-    zeppelin_application_configuration {
-      catalog_configuration {
-        glue_data_catalog_configuration {
-          database_arn   = aws_glue_catalog_database.big_crime_catalog.arn
-        }
-      }
-    }
-  }
-}
+# resource "aws_kinesisanalyticsv2_application" "big_crime_flink" {
+#   name                   = "big-crime-flink-application"
+# #   runtime_environment    = "FLINK-1_18"
+# #   application_mode       = "STREAMING"
+#   runtime_environment    = "ZEPPELIN-FLINK-3_0"
+#   service_execution_role = "arn:aws:iam::780087431924:role/LabRole"
+#   application_mode       = "INTERACTIVE"
+#   application_configuration {
+#     zeppelin_application_configuration {
+#       catalog_configuration {
+#         glue_data_catalog_configuration {
+#           database_arn   = aws_glue_catalog_database.big_crime_catalog.arn
+#         }
+#       }
+#     }
+#   }
+# }
