27e813b1c80c14703e1ee73f326fddcf246a7102
igor.podobinski <igor.podobinski@pl.markant.com>
2024-06-04 12:47:45 +0200

    init terraform 2

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,6 +4,13 @@ terraform {
     key    = "big-crime/tfstate"
     region = "us-east-1"
   }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.52.0"
+    }
+  }
+  required_version = ">= 1.1.0"
 }
 
 provider "aws" {
