6b89eff2d88ce71d6f22c7c88b314820dd8956df
igor.podobinski <igor.podobinski@pl.markant.com>
2024-06-04 10:23:57 +0200

    added more terraform resources

---
+++ infrastructure/kinesis_analytics.tf
@@ -0,0 +1,58 @@
+
+resource "aws_kinesis_stream" "big_crime_stream" {
+  name             = "big-crime-stream"
+  shard_count      = 1
+  retention_period = 48
+
+  shard_level_metrics = [
+    "IncomingBytes",
+    "OutgoingBytes",
+  ]
+
+  stream_mode_details {
+    stream_mode = "PROVISIONED"
+  }
+
+  tags = {
+    Environment = "test"
+  }
+}
+
+resource "aws_glue_catalog_database" "big_crime_catalog" {
+  name = "big-crime-glue-db-for-stream"
+}
+
+resource "aws_kinesis_analytics_application" "test_application" {
+  name = "kinesis-analytics-application-test"
+
+  inputs {
+    name_prefix = "test_prefix"
+
+    kinesis_stream {
+      resource_arn = aws_kinesis_stream.big_crime_stream.arn
+      role_arn     = "LabRole"
+    }
+
+    parallelism {
+      count = 1
+    }
+
+    schema {
+      record_columns {
+        mapping  = "$.test"
+        name     = "test"
+        sql_type = "VARCHAR(8)"
+      }
+
+      record_encoding = "UTF-8"
+
+      record_format {
+        mapping_parameters {
+          json {
+            record_row_path = "$"
+          }
+        }
+      }
+    }
+  }
+}
--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -86,26 +86,40 @@ resource "aws_s3_bucket_policy" "big_crime_formatted_data_bucket_bucket_policy" 
 EOF
 }
 
-resource "aws_vpc" "myvpc" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "myvpc"
-  }
-}
+# resource "aws_vpc" "myvpc" {
+#   cidr_block = "10.0.0.0/16"
+#   tags = {
+#     Name = "myvpc"
+#   }
+# }
 
-resource "aws_subnet" "mysubnet" {
-  vpc_id     = aws_vpc.myvpc.id
-  cidr_block = "10.0.1.0/24"
-  tags = {
-    Name = "mysubnet"
-  }
-}
+# resource "aws_subnet" "mysubnet" {
+#   vpc_id     = aws_vpc.myvpc.id
+#   cidr_block = "10.0.1.0/24"
+#   tags = {
+#     Name = "mysubnet"
+#   }
+# }
 
-resource "aws_instance" "nifi" {
-  ami           = "ami-080e1f13689e07408"
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.mysubnet.id
-  tags = {
-    Name = "nifi"
-  }
-}
+# resource "aws_instance" "nifi" {
+#   ami           = "ami-080e1f13689e07408"
+#   instance_type = "t2.micro"
+#   subnet_id     = aws_subnet.mysubnet.id
+#   tags = {
+#     Name = "nifi"
+#   }
+# }
+
+# resource "aws_kinesisanalyticsv2_application" "big-crime-flink" {
+#   name                   = "big-crime-flink-application"
+#   runtime_environment    = "FLINK-1_15"
+#   service_execution_role = aws_iam_role.example.arn
+#   application_mode       = INTERACTIVE
+#   application_configuration {
+#     zeppelin_application_configuration {
+#       catalog_configuration {
+#
+#       }
+#     }
+#   }
+# }
