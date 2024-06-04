c1f7114c8be945ed68bf22c14ea413a731bc76ff
igor.podobinski <igor.podobinski@pl.markant.com>
2024-06-04 11:43:57 +0200

    changed kinesis analytic for managed flink application

--- infrastructure/kinesis_analytics.tf
+++ infrastructure/kinesis_analytics.tf
@@ -22,37 +22,9 @@ resource "aws_glue_catalog_database" "big_crime_catalog" {
   name = "big-crime-glue-db-for-stream"
 }
 
-resource "aws_kinesis_analytics_application" "test_application" {
-  name = "kinesis-analytics-application-test"
-
-  inputs {
-    name_prefix = "test_prefix"
-
-    kinesis_stream {
-      resource_arn = aws_kinesis_stream.big_crime_stream.arn
-      role_arn     = "arn:aws:iam::780087431924:role/LabRole"
-    }
-
-    parallelism {
-      count = 1
-    }
-
-    schema {
-      record_columns {
-        mapping  = "$.test"
-        name     = "test"
-        sql_type = "VARCHAR(8)"
-      }
-
-      record_encoding = "UTF-8"
-
-      record_format {
-        mapping_parameters {
-          json {
-            record_row_path = "$"
-          }
-        }
-      }
-    }
-  }
+resource "aws_kinesisanalyticsv2_application" "big-crime-flink" {
+  name                   = "big-crime-flink-application"
+  runtime_environment    = "FLINK-1_15"
+  service_execution_role = "arn:aws:iam::780087431924:role/LabRole"
+  application_mode       = INTERACTIVE
 }
--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -113,17 +113,3 @@ EOF
 #     Name = "nifi"
 #   }
 # }
-
-# resource "aws_kinesisanalyticsv2_application" "big-crime-flink" {
-#   name                   = "big-crime-flink-application"
-#   runtime_environment    = "FLINK-1_15"
-#   service_execution_role = aws_iam_role.example.arn
-#   application_mode       = INTERACTIVE
-#   application_configuration {
-#     zeppelin_application_configuration {
-#       catalog_configuration {
-#
-#       }
-#     }
-#   }
-# }
