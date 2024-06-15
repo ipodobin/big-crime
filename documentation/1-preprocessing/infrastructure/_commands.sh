f7def602ddb883300495a05bd456f0e97ffe0998
igor.podobinski <igor.podobinski@pl.markant.com>
2024-06-15 10:12:04 +0200

    added final documentation

--- documentation/1-preprocessing/infrastructure/_commands.sh
+++ documentation/1-preprocessing/infrastructure/_commands.sh
@@ -8,10 +8,18 @@ export BIG_CRIME_GLUE_DB_NAME="big-crime-db"
 export LANDING_ZONE_BUCKET_NAME="landing-zone"
 aws s3api create-bucket --bucket $AWS_ACCOUNT_ID-$LANDING_ZONE_BUCKET_NAME --region us-east-1
 envsubst <./landing_zone_bucket_policy.json >resource.json
+aws s3api put-public-access-block \
+    --bucket $AWS_ACCOUNT_ID-$LANDING_ZONE_BUCKET_NAME \
+    --public-access-block-configuration "BlockPublicAcls=false,IgnorePublicAcls=false,BlockPublicPolicy=false,RestrictPublicBuckets=false"
 aws s3api put-bucket-policy --bucket $AWS_ACCOUNT_ID-$LANDING_ZONE_BUCKET_NAME --policy file://./resource.json
 
 export FORMATTED_DATA_BUCKET_NAME="formatted-data"
 aws s3api create-bucket --bucket $AWS_ACCOUNT_ID-$FORMATTED_DATA_BUCKET_NAME --region us-east-1
+envsubst <./formatted_data_bucket_policy.json >resource.json
+aws s3api put-public-access-block \
+    --bucket $AWS_ACCOUNT_ID-$FORMATTED_DATA_BUCKET_NAME \
+    --public-access-block-configuration "BlockPublicAcls=false,IgnorePublicAcls=false,BlockPublicPolicy=false,RestrictPublicBuckets=false"
+aws s3api put-bucket-policy --bucket $AWS_ACCOUNT_ID-$FORMATTED_DATA_BUCKET_NAME --policy file://./resource.json
 
 aws s3api create-bucket --bucket $AWS_ACCOUNT_ID-app --region us-east-1
 envsubst <./01_to_parquet_conversion.py >script.py
